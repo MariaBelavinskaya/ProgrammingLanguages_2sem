91aff6e79598549db978cf122ba6cfd408ef257a
MariaBelavinskaya <113427614+MariaBelavinskaya@users.noreply.github.com>
2023-05-02 12:14:39 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,6 @@
 #include "Point.h"
 #include <string>
+#include <sstream>
 
 miit::figure::Point::Point(const double user_x, const double user_y)
 	:x(user_x), y(user_y)
@@ -18,7 +19,9 @@ double miit::figure::Point::get_y() const
 
 std::string miit::figure::ToString(Point point)
 {
-	return "X: " + std::to_string(point.get_x()) + " Y: " + std::to_string(point.get_y());
+	std::stringstream ss;
+	ss<< "X: "  << (point.get_x()) << " Y: " << (point.get_y());
+	return ss.str();
 }
 
 std::ostream& miit::figure::operator<<(std::ostream& os, Point& point)
