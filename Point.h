fc81bd862432797bd8b59aeca1e31fac3deb3dea
MariaBelavinskaya <113427614+MariaBelavinskaya@users.noreply.github.com>
2023-06-12 00:28:37 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -4,18 +4,15 @@
 
 namespace miit::figure
 {
-	class Point;
-	std::wstring ToString(Point* point);
-	class Point
+	struct Point
 	{
-	public:
 		Point(const double user_x, const double user_y);
 		double get_x() const;
 		double get_y() const;
-
-		friend std::ostream& operator<<(std::ostream& os, Point& point);
-
-	private:
+		friend std::ostream& operator<< (std::ostream& out, const Point& point);
+		friend bool operator==(const Point& lhs, const Point& rhs);
+		std::wstring ToString(const Point& point);
+		std::wstring ToString(Point* point);
 		double x;
 		double y;
 	};
